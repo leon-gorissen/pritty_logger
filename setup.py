f930b90ed1b6f8bb2df72c1ef56277140e1d97d3
Leon Michel Gorißen <leon.gorissen@llt.rwth-aachen.de>
2024-07-12 11:34:04 +0200

    correct versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pritty_logger",
-    version="0.1.2",
+    version="0.2.0",
     description="A logger with rich formatting",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
